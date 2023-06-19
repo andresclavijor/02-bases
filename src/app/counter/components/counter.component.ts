1b3f5655c0e8a4bc469f82f5682ad7e11132aecb
Andres Clavijo~ <shipoca2012@hotmail.com>
2023-06-19 16:44:21 -0500

    ajuste visual

--- src/app/counter/components/counter.component.ts
+++ src/app/counter/components/counter.component.ts
@@ -4,9 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-counter',
   template: `
     <h3>Counter: {{ counter }}</h3>
-    <button (click)="increaseBy(+1)">+1</button>
-    <button (click)="reset()">reset</button>
-    <button (click)="increaseBy(-1)">-1</button>
+    <button class="btn btn-primary mx-2" (click)="increaseBy(+1)">+1</button>
+    <button class="btn btn-primary mx-2" (click)="reset()">reset</button>
+    <button class="btn btn-primary mx-2" (click)="increaseBy(-1)">-1</button>
   `,
 })
 export class CounterComponent implements OnInit {
